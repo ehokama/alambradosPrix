6c5203b03efd2628c84e169afd5032a207dc2d9f
Ezequiel Hokama <ehokamas@gmail.com>
2025-07-30 15:00:51 -0300

    Update Bienvenida.tsx

--- src/components/Bienvenida.tsx
+++ src/components/Bienvenida.tsx
@@ -1,26 +1,29 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '@/firebase/config';
 import { obtenerDocumentoPorCampo } from '@/app/utils/firestoreUtils';
 
-export default function TestFirestore() {
+export default function MensajeBienvenida() {
   const [nombre, setNombre] = useState<string>('');
 
   useEffect(() => {
-    const probar = async () => {
-        type Usuario = {
-            nombre: string;
-            correo: string;
-        };
-      const doc = await obtenerDocumentoPorCampo('usuarios', 'email', 'ehokamas@gmail.com');
-      if (doc && doc.nombre) {
-        setNombre(doc.nombre); 
+    const unsubscribe = onAuthStateChanged(auth, async (usuario) => {
+      if (usuario?.email) {
+        const doc = await obtenerDocumentoPorCampo('usuarios', 'email', usuario.email);
+        if (doc && 'nombre' in doc) {
+          setNombre(doc.nombre);
+        } else {
+          setNombre('');
+        }
+      } else {
+        setNombre('');
       }
-    };
+    });
 
-    probar();
+    return () => unsubscribe();
   }, []);
 
-  return <h1 className="welcome-text">Bienvenido {nombre ? ` ${nombre}` : 'Cargando...'}</h1>
-
+  return <h1 className="welcome-text">Bienvenido {nombre ? nombre : 'Cargando...'}</h1>;
 }
